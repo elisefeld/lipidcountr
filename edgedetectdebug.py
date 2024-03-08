6c32fb7a5db3013c81a9fb27b79bfa841d4b6972
elisefeld <75515626+elisefeld@users.noreply.github.com>
2024-03-08 17:22:05 -0600

    modified:   edgedetectdebug.py

--- edgedetectdebug.py
+++ edgedetectdebug.py
@@ -1,46 +1,41 @@
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
-from PIL import Image, ImageFilter
 import csv
-import glob
 import pandas as pd
 import os
 
-image_name = []
-cell_num = []
+# Initialize Lists
 cells = []
+cell_num = []
+droplets = []
+droplets_count = []
 masks = []
-
-kernel = np.ones((3, 3), np.uint8)
+image_names = []
+file_names = []
 
 file_path = "/Users/Elise/Code/TETLAB/Images/INA/3.4.24/"
-file_names = os.listdir(r"/Users/Elise/Code/TETLAB/Images/INA/3.4.24")
 
-print(file_names)
+# Iterate through images in a given folder.
+for file_name in os.listdir(file_path):
+   if (file_name.endswith(".jpg") | file_name.endswith(".tiff")):
+        file_names.append(file_name)
+
 
 i = 0
-while i <= len(file_names):
+while i < len(file_names):
     
     original = cv.imread(file_path + file_names[i], cv.IMREAD_GRAYSCALE)
-    cv.imwrite("original" + "_file_" + str(i) + ".jpg", original)
+    #cv.imwrite("original" + "_file_" + str(i) + ".jpg", original)
     assert original is not None, "file could not be read, check with os.path.exists()"
 
-    #Gaussian Blur
+    # Preprocessing
     blur = cv.GaussianBlur(original, (3, 3), 0) 
-    cv.imwrite("blur" + "_file_" + str(i) + ".jpg", blur)
-
-    #Adaptive Thresholding
     thresh = cv.adaptiveThreshold(blur, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
-    cv.imwrite("thresh" + "_file_" + str(i) + ".jpg", thresh)
-
-    #Dilation & Erosion Operations
+    kernel = np.ones((3, 3), np.uint8)
     dilated = cv.dilate(thresh, kernel, iterations=1)
-    cv.imwrite("dilated" + "_file_" + str(i) + ".jpg", dilated)
     eroded = cv.erode(dilated, kernel, iterations=1)
-    cv.imwrite("eroded" + "_file_" + str(i) + ".jpg", eroded)
 
-    #Connected Components
+    # Connected Components
     N, connected, statistics, centroids = cv.connectedComponentsWithStats(eroded)
     sizes = statistics[:,-1]
     
@@ -54,46 +49,55 @@ while i <= len(file_names):
 
         mask = mask == 1
         masks.append(mask)
+        
+        cell_num.append(n)
+        image_names.append(file_names[i])
+
+        cell = np.copy(original)
+        cell[~mask.astype(bool)] = 0
+        spots = cv.adaptiveThreshold(cell, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+        circ, hierarchy = cv.findContours(spots, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        droplets_count.append(len(circ))
+
 
     debug = np.zeros(connected.shape, np.uint8)
     for n in range(len(masks)):
         debug[masks[n] == True] = (n + 1) * 8
 
-    for n in range(len(masks)):
-     cell = np.copy(original)
-     cell[~masks[n]] = 0
-     cells.append(cell)
+    # for n in range(len(masks)):
+    #  cell = np.copy(original)
+    #  cell[~masks[n]] = 0
+    #  cells.append(cell)
 
-     for n in cell_num:
-        cell_num.append(n)
-    i += 1
+    # for n in range(1, N):
+    #     spots = cv.adaptiveThreshold(cell, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+    #     circ, hierarchy = cv.findContours(spots, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    #     droplets_count.append(circ)
+
+    # z = 0
+    # while z < len(file_names):
+    #  for n in droplets:
+    #   circ, hierarchy = cv.findContours(n,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+    #   droplets_count.append(len(circ))
+    #   y += 1
         
+    #cv.imwrite("blur" + "_file_" + str(i) + ".jpg", blur)
+    #cv.imwrite("thresh" + "_file_" + str(i) + ".jpg", thresh)
+    #cv.imwrite("dilated" + "_file_" + str(i) + ".jpg", dilated)
+    #cv.imwrite("eroded" + "_file_" + str(i) + ".jpg", eroded)
+
+    i += 1
+
+print(len(cell_num))
+print(len(image_names))
+print(len(droplets_count))
+
+dict = {'images': image_names, 'cells': cell_num, 'droplets': droplets_count}  
+df = pd.DataFrame(dict) 
+df.to_csv('lipid_count.csv') 
 
 
-    # for n in range(len(cells)):
-    #     cv.imshow(f"Cell {n}", cells[n])
 
-
-# droplets = []
-# for image_path in images:
-#     for n in cells:
-#      spot = cv.adaptiveThreshold(cells[n],255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,11,2)
-#      droplets.append(spot)
-
-# droplets_count = []
-# for image_path in images:
-#     for n in droplets:
-#      image, circ, hierarchy = cv.findContours(droplets[n],cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-#      droplets_count.append(len(circ))
-
-
-# dict = {'images': images, 'cells': cell_num, 'droplets': droplets_count}  
-# df = pd.DataFrame(dict) 
-# df.to_csv('lipid_count.csv') 
-
-# print(images)
-# print(cell_num)
-# print(droplets_count)
 
 # # cv.imshow("original", original)
 # # cv.imshow("thresh", thresh)
